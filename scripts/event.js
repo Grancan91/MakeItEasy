8bbe405a07582365c6cdcc14cb9faeb9bf349d1f
Grancan91 <abl.lopper@gmail.com>
2023-04-23 15:22:47 +0100

    ok

--- scripts/event.js
+++ scripts/event.js
@@ -1,55 +1,35 @@
 
 class Event {
-    
+
     constructor(name = "New Event") {
         this.eventId = 1
         this.name = name
         this.dom = HTMLCollection
     }
-   
-    mainEvent(){
+
+    mainEvents() {
         let main = document.querySelector('.main')
         let event = document.querySelector('.event')
         let eventUl = document.querySelector('ul')
         let eventLi = document.querySelector('li')
 
-            if(event){
-            event = document.createElement('div')
-            event.setAttribute('class','event')
-            event.setAttribute('id', this.eventId)
-            
-            eventLi = document.createElement('li')
-            eventLi.appendChild(document.createTextNode("Lista"))
-            eventUl = document.createElement('ul')
-            eventUl.appendChild(eventLi)
-            
-            
-            event.appendChild(eventUl)
-            main.appendChild(event)
-            } else {
-                event = document.createElement('div')
-                event.setAttribute('class','event')
-                event.setAttribute('id', this.eventId)
-                
-                eventLi = document.createElement('li')
-                eventLi.appendChild(document.createTextNode("Dont Have Events Jet"))
-                eventUl = document.createElement('ul')
-                eventUl.appendChild(eventLi)
-                
-                
-                event.appendChild(eventUl)
-                main.appendChild(event)
-            }
+        event = document.createElement('div')
+        event.setAttribute('class', 'event')
+        event.setAttribute('id', this.eventId)
+        eventLi = document.createElement('li')
+        eventLi.appendChild(document.createTextNode(this.name))
+        eventUl = document.createElement('ul')
+        eventUl.appendChild(eventLi)
+        event.appendChild(eventUl)
+        main.appendChild(event)
 
-            
-       
-        
     }
-    //
-  
+
+
+
 }
 
 const event = new Event()
 
-export {event}
+export { event }
 
--- scripts/index.js
+++ scripts/index.js
@@ -6,8 +6,25 @@ const loadNavbar = function(){
 
    //Localizar Div en el HTML
    event.dom = document.getElementsByClassName('navbar')
-   event.mainEvent()
+   event.mainEvents()
    console.log(event.dom)
 
 }
-loadNavbar()
+
+const loadSidebar = function(){
+ /*   
+     let main = document.querySelector('.sidebar')
+     let newEvent = document.querySelector('.new-event')
+     let newImg = document.querySelector('.img')
+    
+     newEvent = document.createElement('div')
+     newEvent.setAttribute('class', 'new-event')
+     newImg = document.createElement('img')
+     newEvent.appendChild(newImg)
+     main.appendChild(newEvent)
+   */  
+    
+}
+
+loadSidebar()
+//loadSidebar()
